8ad83f0c4c83286f8f8d3406309b5c0187a8ca22
Yuvraj Rana <yuvrajrana752@gmail.com>
2024-01-05 15:03:24 +0530

    commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# DevSearch
+
+## Home
+<img src="./resources/Home.png">  
+
+## Projects
+<img src="./resources/Projects.png">  
+
+## Profile
+<img src="./resources/Profile.png">  
+
+## Feedback
+<img src="./resources/Feedback.png">
+
+## Message
+<img src="./resources/Message.png">
+
+## Login
+<img src="./resources/Login.png">  
--- devsearch/settings.py
+++ devsearch/settings.py
@@ -127,12 +127,12 @@ USE_L10N = True
 
 USE_TZ = True
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'YOUR-EMAIL'
-EMAIL_HOST_PASSWORD = 'YOUR-EMAIL-PASSWORD'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = 'YOUR-EMAIL'
+# EMAIL_HOST_PASSWORD = 'YOUR-EMAIL-PASSWORD'
 
 
 # Static files (CSS, JavaScript, Images)
